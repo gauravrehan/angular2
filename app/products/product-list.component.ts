1f2626cbbb6a2cc17089c9584b05f4b932706beb
Gaurav Rehan <gaurav.rehan@gmail.com>
2017-07-22 22:09:02 +0530

    Nested Components

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -82,7 +82,7 @@ export class ProductListComponent implements OnInit {
 toggleImage(): void {this.showImage = !this.showImage;};
 
 onRatingClicked(message: string) : void {
-    this.message = message;
+    this.pageTitle = 'Product List: ' + message;
 }
 
 }
